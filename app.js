04bd5668117a6e6adb166d2709f7466637fd7c67
Thawatchai <thawatchai.muenrit@gmail.com>
2023-02-16 14:59:13 +0700

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const productRouter = express.Router();
 
 const app = express();
 const port = process.env.PORT;
@@ -9,8 +10,21 @@ const port = process.env.PORT;
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
 
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+productRouter.route("/").get((req, res) => {
+  res.render("products");
+});
+
+productRouter.route("/1").get((req, res) => {
+  res.send("Hello World!! I'm Product 1");
+});
+
+app.use("/products", productRouter);
+
 app.get("/", (req, res) => {
-  res.send("Hello xTWCXx");
+  res.render("index", { username: "TWC", customers: ["Thon", "Teen", "Gone"] });
 });
 
 app.listen(port, () => {
