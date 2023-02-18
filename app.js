80b3f41ce88f0724248290926e34b50fd02754f1
Thawatchai <thawatchai.muenrit@gmail.com>
2023-02-18 21:37:17 +0700

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const products = require("./data/products.json");
 const productRouter = express.Router();
 
 const app = express();
@@ -14,18 +15,12 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 productRouter.route("/").get((req, res) => {
-  res.render("products", {
-    products: [
-      { title: "RTX4090", description: "การ์ดจอ RTX4090", price: "49900" },
-      { title: "RTX4080", description: "การ์ดจอ RTX4080", price: "39900" },
-      { title: "RTX4070", description: "การ์ดจอ RTX4070", price: "29900" },
-      { title: "RTX4060", description: "การ์ดจอ RTX4060", price: "19900" },
-    ],
-  });
+  res.render("products", { products });
 });
 
-productRouter.route("/1").get((req, res) => {
-  res.send("Hello World!! I'm Product 1");
+productRouter.route("/:id").get((req, res) => {
+  const id = req.params.id;
+  res.render("product", { product: products[id] });
 });
 
 app.use("/products", productRouter);
