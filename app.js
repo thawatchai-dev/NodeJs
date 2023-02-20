632386129d1abbddf54eeaae512cbff6530927eb
Thawatchai <thawatchai.muenrit@gmail.com>
2023-02-20 14:49:50 +0700

    20/02/2023

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
-const products = require("./data/products.json");
-const productRouter = express.Router();
 
 const app = express();
 const port = process.env.PORT;
+const productsRouter = require("./src/router/productsRouter");
 
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -14,16 +13,7 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) => {
-  res.render("products", { products });
-});
-
-productRouter.route("/:id").get((req, res) => {
-  const id = req.params.id;
-  res.render("product", { product: products[id] });
-});
-
-app.use("/products", productRouter);
+app.use("/products", productsRouter);
 
 app.get("/", (req, res) => {
   res.render("index", { username: "TWC", customers: ["Thon", "Teen", "Gone"] });
